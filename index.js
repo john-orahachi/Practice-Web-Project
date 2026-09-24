6e4e9fb9e5fc8955edaa69dbb2e2643a40fbced8
john-orahachi <jjjonjos13@gmail.com>
2026-09-24 10:18:21 +0100

    added the dark mode button without styling

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const navLinks = document.getElementById('navLinks');
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+const darkMode = document.getElementById("darkMode").onclick(() =>{
+  document.body.classList.toggle("dark-mode")
+});
+
+if(document.body.className === "dark-mode"){
+  darkMode.textContent = "Light Mode";
+}
