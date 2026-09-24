7a516b4d4732ac2eb477ac6c90d4e6bcb13b1e18
john-orahachi <jjjonjos13@gmail.com>
2026-09-24 12:17:31 +0100

    updated the darkmode features but its still incomplete for now

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const navLinks = document.getElementById('navLinks');
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+const bodyElement = document.getElementById("body-element");
+const darkMode = document.getElementById("darkMode");
 
-const darkMode = document.getElementById("darkMode").onclick(() =>{
-  document.body.classList.toggle("dark-mode")
+darkMode.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark-mode");
+  if(bodyElement.classList.contains("dark-mode")){
+    darkMode.textContent = "Light Mode";
+    darkMode.style.backgroundColor = "white";
+    darkMode.style.color = "black";
+    bodyElement.style.color = "black";
+    document.div.style.backgroundColor = "black";
+  }
+  else {
+    darkMode.textContent = "Dark Mode";
+    darkMode.style.backgroundColor = "black";
+    darkMode.style.color = "white";
+  }
 });
-
-if(document.body.className === "dark-mode"){
-  darkMode.textContent = "Light Mode";
-}
